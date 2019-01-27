d520e0d73233dacf26fcbaf2cc1f9559d8cc1521
dulcineapena1 <f.dulcinea.pena@gmail.com>
2019-01-27 12:07:22 -0600

    seed ordenes y proyectos separado

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -2,6 +2,74 @@ const mongoose = require("mongoose")
 const db = require("../models")
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/labdb")
+const ordenesSeed=[{
+  clave: "814-FF-18-1",
+  rama: "FF",
+  tipodeestudio: "206",
+  start: "Tue Aug 07 2018",
+  end: "Tue Aug 07 2018",
+  metodos: [
+    "NMX-AA-009-1993, SCFI",
+    "NMX-AA-010-SCFI-2001",
+    "NMX-AA-035-1976",
+    "NMX-AA-054-1978",
+    "EPA METODO 3A-2008",
+    "EPA METODO 6C-2008",
+    "EPA METODO 7E-2008",
+    "EPA METODO 10-2008"
+  ],
+  signatario: ["AYB", "JMGA"],
+  equipo: ["Equipo1", "Equipo2"],
+  status: "Payed",
+  preciosubtotal: "5000"
+},
+
+{
+  clave: "814-FF-18-3",
+  rama: "FF",
+  tipodeestudio: "206",
+  start: "Tue Aug 07 2018",
+  end: "Tue Aug 07 2018",
+  metodos: [
+    "NMX-AA-009-1993, SCFI",
+    "NMX-AA-010-SCFI-2001",
+    "NMX-AA-035-1976",
+    "NMX-AA-054-1978",
+    "EPA METODO 3A-2008",
+    "EPA METODO 6C-2008",
+    "EPA METODO 7E-2008",
+    "EPA METODO 10-2008"
+  ],
+  signatario: ["AYB"],
+  equipo: ["Equipo1", "Equipo2"],
+  status: "Payed",
+  preciosubtotal: "2000"
+},
+{
+  clave: "814-AR-18-1",
+  rama: "AR",
+  tipodeestudio: "206",
+  start: "Wed Aug 08 2018",
+  end: "Wed Aug 08 2018",
+  metodos: [
+    "NMX-AA-009-1993, SCFI",
+    "NMX-AA-010-SCFI-2001",
+    "NMX-AA-035-1976",
+    "NMX-AA-054-1978",
+    "EPA METODO 3A-2008",
+    "EPA METODO 6C-2008",
+    "EPA METODO 7E-2008",
+    "EPA METODO 10-2008"
+  ],
+  signatario: ["AYB"],
+  equipo: ["Equipo1", "Equipo2"],
+  status: "Payed",
+  preciosubtotal: "2000"
+}
+
+
+]
+
 const proyectSeed = [
   {
     clave: "814",
@@ -92,8 +160,8 @@ const proyectSeed = [
         clave: "830-AR-18-1",
         rama: "AR",
         tipodeestudio: "206",
-        start: "Mon Aug 13 2018",
-        end: "Mon Aug 13 2018",
+        inicio: "Mon Aug 13 2018",
+        fin: "Mon Aug 13 2018",
         metodos: [
           "NMX-AA-009-1993, SCFI",
           "NMX-AA-010-SCFI-2001",
@@ -113,8 +181,8 @@ const proyectSeed = [
         clave: "830-FF-18-1",
         rama: "FF",
         tipodeestudio: "206",
-        start: "Tue Aug 14 2018",
-        end: "Tue Aug 14 2018",
+        inicio: "Tue Aug 14 2018",
+        fin: "Tue Aug 14 2018",
         metodos: [
           "NMX-AA-009-1993, SCFI",
           "NMX-AA-010-SCFI-2001",
@@ -263,10 +331,12 @@ const proyectSeed = [
   }
 ]
 
+
+
 db.Proyectos.remove({})
 db.Ordenes.remove({})
   .then(() => db.Proyectos.collection.insertMany(proyectSeed))
-
+  .then(() => db.Ordenes.collection.insertMany(ordenesSeed))
   .then(data => {
     console.log(data.result.n + " records inserted!")
     process.exit(0)

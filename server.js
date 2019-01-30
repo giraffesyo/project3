c8da2405ffbe2af5149e0eb580c125db8ff69d6a
Jessica Y <desh365@hotmail.com>
2019-01-30 02:00:51 -0600

    Fix startup scripts

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require("express")
 const mongoose = require("mongoose")
 const routes = require("./routes")
 const app = express()
-const PORT = process.env.PORT || 3001
+const PORT =
+  process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 5000
 
 //Resuelve problema de debatirse entre el puerto 3000 para front y 3001 para apiroutes
 /*var allowCrossDomain = function(req, res, next) {

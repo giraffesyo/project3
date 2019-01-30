b0b69caa8aee9cdd930eb0278fe11bfa916a706f
dulcineapena1 <f.dulcinea.pena@gmail.com>
2019-01-29 20:21:52 -0600

    calendario propiedades

--- client/src/components/section/calendario.js
+++ client/src/components/section/calendario.js
@@ -23,18 +23,59 @@ class Section extends Compo {
         {
           start: new Date(),
           end: new Date(moment().add(1, "days")),
+          color:"",
           title: "Some title",
           //allDay: true     Para eventos de todo el día
           //desc: 'Big conference for important people'   Para agregar info adicional
-        }
+        },
+        {
+          title: 'on DST',
+          start: new Date(2019, 2, 1, 1),
+          end: new Date(2019, 2, 1, 2, 30),
+          allDay: false,
+        },
+        {
+          title: 'crosses DST',
+          start: new Date(2019, 2, 1, 5),
+          end: new Date(2019, 2, 1, 6, 30),
+          allDay: false,
+        },
+        {
+          title: 'MultiDay 1',
+          start: new Date(2019, 3, 24),
+          color: '#dc143c',
+          end: new Date(2019, 3, 25, 1, 0, 0, 0),
+        },
+        {
+          title: 'MultiDay 2',
+          start: new Date(2019, 3, 25),
+         
+          end: new Date(2019, 3, 26, 6, 0, 0, 0),
+        },
+        {
+          title: 'has time',
+          start: moment(new Date(2016, 11, 3))
+            .add(5, 'days')
+            .subtract(5, 'hours')
+            .toDate(),
+          end: moment(new Date(2016, 11, 3))
+            .add(1, 'days')
+            .subtract(4, 'hours')
+            .toDate(),
+        },
       ]
   };
 
-
+  // metodos: this.state.detalleUnSignatario.metodos.filter(function(person) { 
+            //     return person !== metodoABorrar.nombre 
+            //     }),
 
   componentDidMount = () => {
+    const seleccionarColorRandom=('#' + Math.floor(Math.random()*16777215).toString(16))
+    console.log("color random",seleccionarColorRandom)
     API.getProyect()
-    .then(res => this.setState({ events: this.state.events.concat(res.data) }))
+    .then(res => this.setState({ events: this.state.events.concat(res.data)}
+    )) .then(res=>this.setState(state=>{state.events[0].color= seleccionarColorRandom} )  )
     .catch(err => console.log(err));
   }
 
@@ -76,13 +117,26 @@ class Section extends Compo {
     //console.log(event)
   }
 
+  
 
 
   render() {
-      
+    // let formats = {
+    //   dateFormat: 'dd',
+    
+    //   dayFormat: (date,  localizer) =>
+    //     localizer.format(date, 'DDD', culture),
+    
+    //   dayRangeHeaderFormat: ({ start, end }, culture, localizer) =>
+    //     localizer.format(start, { date: 'short' }, culture) + ' — ' +
+    //     localizer.format(end, { date: 'short' }, culture)
+    // }  
+
+
     return (
       <div className="contenedorcalendario">
         <DnDCalendar 
+          popup //Para extender cuando hay varios eventos en un mismo día
           selectable //Hace posible que se pueda seleccionar
           onSelectSlot={this.handleSelect}
           onSelectEvent={event => alert(event.nombreempresa + event.direccion)} //Al hacer click sobre el evento, despliega info del evento
@@ -94,6 +148,9 @@ class Section extends Compo {
           defaultView="month"
           events={this.state.events}
           style={{ height: "70vh" }} //Sin esta línea no se muestra el calendario
+          eventPropGetter={event => ({ style: {backgroundColor: event.color} })} //Añadir props a los events, ej. cambiar color
+          culture="es"
+          // formats={formats}//Formato de fecha e idioma
         />
       </div>
     );
--- models/ordenes.js
+++ models/ordenes.js
@@ -13,7 +13,8 @@ const OrdenesSchema = new Schema({
   end: { type: Date, default: Date.now },
   // comentarios: { type: String },
   status: { type: String },
-  preciosubtotal: { type: Number } //Le puse precio total, para que en cada orden haya precio subtotal (el total es la suma de los subtotales)
+  preciosubtotal: { type: Number }, //Le puse precio total, para que en cada orden haya precio subtotal (el total es la suma de los subtotales)
+  color:{ type: String }
 })
 
 const Ordenes = mongoose.model("Ordenes", OrdenesSchema)

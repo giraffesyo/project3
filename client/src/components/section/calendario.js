c0a91e8e6f6ee9b3cc10002638ce485ac1802044
dulcineapena1 <f.dulcinea.pena@gmail.com>
2019-01-30 17:40:56 -0600

    getOrdenes en calendario

--- client/src/components/section/calendario.js
+++ client/src/components/section/calendario.js
@@ -21,7 +21,8 @@ class Section extends Compo {
         start: new Date(),
         end: new Date(moment().add(1, "days")),
         color: "",
-        title: "Some title"
+        title: "Some title",
+        resourceId: 1,
         //allDay: true     Para eventos de todo el día
         //desc: 'Big conference for important people'   Para agregar info adicional
       },
@@ -31,57 +32,62 @@ class Section extends Compo {
       //   end: new Date(2019, 2, 1, 2, 30),
       //   allDay: false,
       // },
-      {
-        title: "crosses DST",
-        start: new Date(2019, 2, 1, 5),
-        end: new Date(2019, 2, 1, 6, 30),
-        allDay: false
-      },
-      {
-        title: "MultiDay 1",
-        start: new Date(2019, 3, 24),
-        color: "#dc143c",
-        end: new Date(2019, 3, 25, 1, 0, 0, 0)
-      },
-      {
-        title: "MultiDay 2",
-        start: new Date(2019, 3, 25),
+      // {
+      //   title: "crosses DST",
+      //   start: new Date(2019, 2, 1, 5),
+      //   end: new Date(2019, 2, 1, 6, 30),
+      //   allDay: false
+      // },
+      // {
+      //   title: "MultiDay 1",
+      //   start: new Date(2019, 3, 24),
+      //   color: "#dc143c",
+      //   end: new Date(2019, 3, 25, 1, 0, 0, 0)
+      // },
+      // {
+      //   title: "MultiDay 2",
+      //   start: new Date(2019, 3, 25),
 
-        end: new Date(2019, 3, 26, 6, 0, 0, 0)
-      },
-      {
-        title: "has time",
-        start: moment(new Date(2016, 11, 3))
-          .add(5, "days")
-          .subtract(5, "hours")
-          .toDate(),
-        end: moment(new Date(2016, 11, 3))
-          .add(1, "days")
-          .subtract(4, "hours")
-          .toDate()
-      }
+      //   end: new Date(2019, 3, 26, 6, 0, 0, 0)
+      // },
+      // {
+      //   title: "has time",
+      //   start: moment(new Date(2016, 11, 3))
+      //     .add(5, "days")
+      //     .subtract(5, "hours")
+      //     .toDate(),
+      //   end: moment(new Date(2016, 11, 3))
+      //     .add(1, "days")
+      //     .subtract(4, "hours")
+      //     .toDate()
+      // }
     ]
   }
 
-  // metodos: this.state.detalleUnSignatario.metodos.filter(function(person) {
-  //     return person !== metodoABorrar.nombre
-  //     }),
+
+  
 
   componentDidMount = () => {
-    const seleccionarColorRandom =
-      "#" + Math.floor(Math.random() * 16777215).toString(16)
-    console.log("color random", seleccionarColorRandom)
-    API.getProyect()
-      .then(res =>
-        this.setState({ events: this.state.events.concat(res.data) })
-      )
-      .then(res =>
-        this.setState(state => {
-          state.events[0].color = seleccionarColorRandom
-        })
-      )
+    API.getOrden()
+      .then(res => this.setState({ events: this.state.events.concat(res.data) }) )
       .catch(err => console.log(err))
   }
+  
+  // componentDidMount = () => {
+  //   const seleccionarColorRandom =
+  //     "#" + Math.floor(Math.random() * 16777215).toString(16)
+  //   console.log("color random", seleccionarColorRandom)
+  //   API.getProyect()
+  //     .then(res =>
+  //       this.setState({ events: this.state.events.concat(res.data) })
+  //     )
+  //     .then(res =>
+  //       this.setState(state => {
+  //         state.events[0].color = seleccionarColorRandom
+  //       })
+  //     )
+  //     .catch(err => console.log(err))
+  // }
 
   //-->Para arrastrar y cambiar el start y end del evento
   // El "event" contiene todo el evento
@@ -117,24 +123,31 @@ class Section extends Compo {
   }
 
   render() {
+    // const culture="es"
     // let formats = {
     //   dateFormat: 'dd',
 
-    //   dayFormat: (date,  localizer) =>
-    //     localizer.format(date, 'DDD', culture),
+    //   dayFormat: (date,  localizer,culture) =>
+    //     localizer.format(date, 'DDD', culture={culture:'es'}),
 
     //   dayRangeHeaderFormat: ({ start, end }, culture, localizer) =>
     //     localizer.format(start, { date: 'short' }, culture) + ' — ' +
     //     localizer.format(end, { date: 'short' }, culture)
     // }
 
+    const resourceMap = [
+      { resourceId: 1, resourceTitle: 'Board room' },
+      { resourceId: 2, resourceTitle: 'Training room' },
+      { resourceId: 3, resourceTitle: 'Meeting room 1' },
+      { resourceId: 4, resourceTitle: 'Meeting room 2' },
+    ]
     return (
       <div className="contenedorcalendario">
         <DnDCalendar
           popup //Para extender cuando hay varios eventos en un mismo día
           selectable //Hace posible que se pueda seleccionar
           onSelectSlot={this.handleSelect}
-          onSelectEvent={event => alert(event.nombreempresa + event.direccion)} //Al hacer click sobre el evento, despliega info del evento
+          onSelectEvent={event => alert(event.clave + event.tipodeestudio)} //Al hacer click sobre el evento, despliega info del evento
           onEventDrop={this.onEventDrop}
           onEventResize={this.onEventResize}
           localizer={localizer}
@@ -145,8 +158,12 @@ class Section extends Compo {
           eventPropGetter={event => ({
             style: { backgroundColor: event.color }
           })} //Añadir props a los events, ej. cambiar color
-          culture="es"
-          // formats={formats}//Formato de fecha e idioma
+          //  formats={formats}//Formato de fecha e idioma
+          //   culture="es"
+          // views={["month", "day"]}
+          resources={resourceMap}
+          resourceIdAccessor="resourceId"
+          resourceTitleAccessor="resourceTitle"
         />
       </div>
     )
---
+++ scripts/seedDB3.js
@@ -0,0 +1,355 @@
+const mongoose = require("mongoose")
+const db = require("../models")
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/labdb")
+
+
+
+
+const ordenesSeed = [
+  {
+    title: "814-FF-18-1",
+    proyecto: "444444444",
+    clave: "814-FF-18-1",
+    rama: "FF",
+    tipodeestudio: "206",
+    start: "Tue Aug 07 2018",
+    end: "Tue Aug 07 2018",
+    metodos: [
+      "NMX-AA-009-1993, SCFI",
+      "NMX-AA-010-SCFI-2001",
+      "NMX-AA-035-1976",
+      "NMX-AA-054-1978",
+      "EPA METODO 3A-2008",
+      "EPA METODO 6C-2008",
+      "EPA METODO 7E-2008",
+      "EPA METODO 10-2008"
+    ],
+    signatario: ["AYB", "JMGA"],
+    equipo: ["Equipo1", "Equipo2"],
+    status: "Payed",
+    preciosubtotal: "5000",
+    color:"#1e6438"
+  },
+
+  {
+    clave: "814-FF-18-2",
+    rama: "FF",
+    tipodeestudio: "206",
+    start: "Tue Aug 07 2018",
+    end: "Tue Aug 07 2018",
+    metodos: [
+      "NMX-AA-009-1993, SCFI",
+      "NMX-AA-010-SCFI-2001",
+      "NMX-AA-035-1976",
+      "NMX-AA-054-1978",
+      "EPA METODO 3A-2008",
+      "EPA METODO 6C-2008",
+      "EPA METODO 7E-2008",
+      "EPA METODO 10-2008"
+    ],
+    signatario: ["AYB"],
+    equipo: ["Equipo1", "Equipo2"],
+    status: "Payed",
+    preciosubtotal: "2000",
+    color:"#1e6438"
+  },
+  {
+    clave: "814-AR-18-1",
+    rama: "AR",
+    tipodeestudio: "206",
+    start: "Wed Aug 08 2018",
+    end: "Wed Aug 08 2018",
+    metodos: [
+      "NMX-AA-009-1993, SCFI",
+      "NMX-AA-010-SCFI-2001",
+      "NMX-AA-035-1976",
+      "NMX-AA-054-1978",
+      "EPA METODO 3A-2008",
+      "EPA METODO 6C-2008",
+      "EPA METODO 7E-2008",
+      "EPA METODO 10-2008"
+    ],
+    signatario: ["AYB"],
+    equipo: ["Equipo1", "Equipo2"],
+    status: "Payed",
+    preciosubtotal: "2000",
+    color:"#984362"
+  }
+]
+
+const proyectSeed = [
+  {
+    title: "814",
+    clave: "814",
+    nombreempresa: "Asofarma de México, S.A de C.V",
+    direccion:
+      "Calz Mexico Xochimilco 43, Col. San Lorenzo Huipulco, Tlalpan, C.P 14370",
+    start: "Tue Aug 07 2018",
+    end: "Wed Aug 08 2018",
+    preciototal: "8000",
+    cantidad: "4",
+    // ordenes: [
+    //   {
+    //     clave: "814-FF-18-1",
+    //     rama: "FF",
+    //     tipodeestudio: "206",
+    //     start: "Tue Aug 07 2018",
+    //     end: "Tue Aug 07 2018",
+    //     metodos: [
+    //       "NMX-AA-009-1993, SCFI",
+    //       "NMX-AA-010-SCFI-2001",
+    //       "NMX-AA-035-1976",
+    //       "NMX-AA-054-1978",
+    //       "EPA METODO 3A-2008",
+    //       "EPA METODO 6C-2008",
+    //       "EPA METODO 7E-2008",
+    //       "EPA METODO 10-2008"
+    //     ],
+    //     signatario: ["AYB", "JMGA"],
+    //     equipo: ["Equipo1", "Equipo2"],
+    //     status: "Payed",
+    //     preciosubtotal: "5000"
+    //   },
+    //   {
+    //     clave: "814-FF-18-3",
+    //     rama: "FF",
+    //     tipodeestudio: "206",
+    //     start: "Tue Aug 07 2018",
+    //     end: "Tue Aug 07 2018",
+    //     metodos: [
+    //       "NMX-AA-009-1993, SCFI",
+    //       "NMX-AA-010-SCFI-2001",
+    //       "NMX-AA-035-1976",
+    //       "NMX-AA-054-1978",
+    //       "EPA METODO 3A-2008",
+    //       "EPA METODO 6C-2008",
+    //       "EPA METODO 7E-2008",
+    //       "EPA METODO 10-2008"
+    //     ],
+    //     signatario: ["AYB"],
+    //     equipo: ["Equipo1", "Equipo2"],
+    //     status: "Payed",
+    //     preciosubtotal: "2000"
+    //   },
+    //   {
+    //     clave: "814-AR-18-1",
+    //     rama: "AR",
+    //     tipodeestudio: "206",
+    //     start: "Wed Aug 08 2018",
+    //     end: "Wed Aug 08 2018",
+    //     metodos: [
+    //       "NMX-AA-009-1993, SCFI",
+    //       "NMX-AA-010-SCFI-2001",
+    //       "NMX-AA-035-1976",
+    //       "NMX-AA-054-1978",
+    //       "EPA METODO 3A-2008",
+    //       "EPA METODO 6C-2008",
+    //       "EPA METODO 7E-2008",
+    //       "EPA METODO 10-2008"
+    //     ],
+    //     signatario: ["AYB"],
+    //     equipo: ["Equipo1", "Equipo2"],
+    //     status: "Payed",
+    //     preciosubtotal: "2000"
+    //   }
+    // ]
+  },
+  {
+    title:"830",
+    clave: "830",
+    nombreempresa: "Hospital General Balbuena S.S de la CDMX",
+    direccion:
+      "Calz de Cecilio Robelo y Sur 103, Col. Aeronautica Militar, Venustiano Carranza, C.P 15970",
+    start: "Mon Aug 13 2018",
+    end: "Tue Aug 14 2018",
+    preciototal: "8000",
+    cantidad: "2",
+    // ordenes: [
+    //   {
+    //     clave: "830-AR-18-1",
+    //     rama: "AR",
+    //     tipodeestudio: "206",
+    //     inicio: "Mon Aug 13 2018",
+    //     fin: "Mon Aug 13 2018",
+    //     metodos: [
+    //       "NMX-AA-009-1993, SCFI",
+    //       "NMX-AA-010-SCFI-2001",
+    //       "NMX-AA-035-1976",
+    //       "NMX-AA-054-1978",
+    //       "EPA METODO 3A-2008",
+    //       "EPA METODO 6C-2008",
+    //       "EPA METODO 7E-2008",
+    //       "EPA METODO 10-2008"
+    //     ],
+    //     signatario: ["JMGA"],
+    //     equipo: ["Equipo3", "Equipo4"],
+    //     status: "Payed",
+    //     preciosubtotal: "5000"
+    //   },
+    //   {
+    //     clave: "830-FF-18-1",
+    //     rama: "FF",
+    //     tipodeestudio: "206",
+    //     inicio: "Tue Aug 14 2018",
+    //     fin: "Tue Aug 14 2018",
+    //     metodos: [
+    //       "NMX-AA-009-1993, SCFI",
+    //       "NMX-AA-010-SCFI-2001",
+    //       "NMX-AA-035-1976",
+    //       "NMX-AA-054-1978",
+    //       "EPA METODO 3A-2008",
+    //       "EPA METODO 6C-2008",
+    //       "EPA METODO 7E-2008",
+    //       "EPA METODO 10-2008"
+    //     ],
+    //     signatario: ["JMGA"],
+    //     equipo: ["Equipo3", "Equipo4"],
+    //     status: "Payed",
+    //     preciosubtotal: "2000"
+    //   }
+    // ]
+  },
+  {
+    clave: "831",
+    nombreempresa: "Asofarma de México, S.A de C.V",
+    direccion:
+      "Calz Mexico Xochimilco 43, Col. San Lorenzo Huipulco, Tlalpan, C.P 14370",
+    inicio: "Tue Aug 07 2018",
+    fin: "Wed Aug 08 2018",
+    preciototal: "8000",
+    cantidad: "4",
+    ordenes: [
+      {
+        clave: "831-FF-18-1",
+        rama: "FF",
+        tipodeestudio: "206",
+        start: "Tue Aug 07 2018",
+        end: "Tue Aug 07 2018",
+        metodos: [
+          "NMX-AA-009-1993, SCFI",
+          "NMX-AA-010-SCFI-2001",
+          "NMX-AA-035-1976",
+          "NMX-AA-054-1978",
+          "EPA METODO 3A-2008",
+          "EPA METODO 6C-2008",
+          "EPA METODO 7E-2008",
+          "EPA METODO 10-2008"
+        ],
+        signatario: ["AYB", "JMGA"],
+        equipo: ["Equipo1", "Equipo2"],
+        status: "Payed",
+        preciosubtotal: "5000"
+      },
+      {
+        clave: "831-AR-18-1",
+        rama: "AR",
+        tipodeestudio: "206",
+        start: "Tue Aug 07 2018",
+        end: "Tue Aug 07 2018",
+        metodos: [
+          "NMX-AA-009-1993, SCFI",
+          "NMX-AA-010-SCFI-2001",
+          "NMX-AA-035-1976",
+          "NMX-AA-054-1978",
+          "EPA METODO 3A-2008",
+          "EPA METODO 6C-2008",
+          "EPA METODO 7E-2008",
+          "EPA METODO 10-2008"
+        ],
+        signatario: ["AYB"],
+        equipo: ["Equipo1", "Equipo2"],
+        status: "Payed",
+        preciosubtotal: "2000"
+      },
+      {
+        clave: "831-FF-18-4",
+        rama: "FF",
+        tipodeestudio: "206",
+        start: "Wed Aug 08 2018",
+        end: "Wed Aug 08 2018",
+        metodos: [
+          "NMX-AA-009-1993, SCFI",
+          "NMX-AA-010-SCFI-2001",
+          "NMX-AA-035-1976",
+          "NMX-AA-054-1978",
+          "EPA METODO 3A-2008",
+          "EPA METODO 6C-2008",
+          "EPA METODO 7E-2008",
+          "EPA METODO 10-2008"
+        ],
+        signatario: ["AYB"],
+        equipo: ["Equipo1", "Equipo2"],
+        status: "Payed",
+        preciosubtotal: "2000"
+      }
+    ]
+  },
+  {
+    clave: "850",
+    nombreempresa: "Hospital General Balbuena S.S de la CDMX",
+    direccion:
+      "Calz de Cecilio Robelo y Sur 103, Col. Aeronautica Militar, Venustiano Carranza, C.P 15970",
+    inicio: "Mon Aug 13 2018",
+    fin: "Tue Aug 14 2018",
+    preciototal: "8000",
+    cantidad: "2",
+    ordenes: [
+      {
+        clave: "850-FF-18-1",
+        rama: "FF",
+        tipodeestudio: "206",
+        start: "Mon Aug 13 2018",
+        end: "Mon Aug 13 2018",
+        metodos: [
+          "NMX-AA-009-1993, SCFI",
+          "NMX-AA-010-SCFI-2001",
+          "NMX-AA-035-1976",
+          "NMX-AA-054-1978",
+          "EPA METODO 3A-2008",
+          "EPA METODO 6C-2008",
+          "EPA METODO 7E-2008",
+          "EPA METODO 10-2008"
+        ],
+        signatario: ["JMGA"],
+        equipo: ["Equipo3", "Equipo4"],
+        status: "Payed",
+        preciosubtotal: "5000"
+      },
+      {
+        clave: "850-AR-18-2",
+        rama: "AR",
+        tipodeestudio: "206",
+        start: "Tue Aug 14 2018",
+        end: "Tue Aug 14 2018",
+        metodos: [
+          "NMX-AA-009-1993, SCFI",
+          "NMX-AA-010-SCFI-2001",
+          "NMX-AA-035-1976",
+          "NMX-AA-054-1978",
+          "EPA METODO 3A-2008",
+          "EPA METODO 6C-2008",
+          "EPA METODO 7E-2008",
+          "EPA METODO 10-2008"
+        ],
+        signatario: ["JMGA"],
+        equipo: ["Equipo3", "Equipo4"],
+        status: "Payed",
+        preciosubtotal: "2000"
+      }
+    ]
+  }
+]
+
+// db.Proyectos.remove({})
+db.Ordenes.remove({})
+  // .then(() => db.Proyectos.collection.insertMany(proyectSeed))
+  .then(() => db.Ordenes.collection.insertMany(ordenesSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!")
+    process.exit(0)
+  })
+  .catch(err => {
+    console.error(err)
+    process.exit(1)
+  })
